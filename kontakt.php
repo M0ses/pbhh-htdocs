33d51921af602f110a9bb47b4b416fe6e6bcb71f
Frank Schreiner <schreiner@suse.de>
2017-04-23 16:30:32 +0200

    various changes in kontakte
    * changed email address
    * bigger fonts for lg/md screens
    * general code cleanup

--- kontakt.php
+++ kontakt.php
@@ -1,39 +1,29 @@
-    <div class=row style="padding-top:20px">
+<div id=contact>
+    <div class=row id=address_row>
       <div class="col-xs-10 col-lg-10">
-	    <div>
-	      <div><h4>Georg-Glötzl-Str. 36a</h4></div>
-	      <div><h4>93354 Siegenburg</h4></div>
-	    </div>
+	 <div id=address_1>Georg-Glötzl-Str. 36a</div>
+	 <div id=address_2>93354 Siegenburg</div>
       </div>
-      <div class="col-xs-2 col-lg-2">
-        <p>
-	<a target=_blank href="https://www.google.com/maps/place/Planungsb%C3%BCro+HEZOG/@48.75067,11.84535,16z/data=!4m5!3m4!1s0x0:0xd589309a11ea2897!8m2!3d48.75067!4d11.84535">i
-          <span class="glyphicon glyphicon-road pull-right" style="font-size: 2em">
+      <div class="col-xs-2 col-lg-2" id=address_ico>
+	<a target=_blank href="https://www.google.com/maps/place/Planungsb%C3%BCro+HEZOG/@48.75067,11.84535,16z/data=!4m5!3m4!1s0x0:0xd589309a11ea2897!8m2!3d48.75067!4d11.84535">
+          <span class="glyphicon glyphicon-road pull-right">
           </span>
         </a>
-       </p>
       </div>
     </div>
-    <div style="padding-top:20px;padding-bottom:10px;">
-      <div class="row">
-        <p>
-	  <label class="control-label col-xs-4">Telefon</label>
-	  <div class="col-xs-6">+49 9444 97 22 92</div>
-	  <div class="col-xs-2"><a href="tel:+49-9444-97-22-92"><span class="glyphicon glyphicon-earphone pull-right" style="font-size: 2em"></span></a></div>
-        </p>
-      </div>
-      <div class="row">
-        <p>
-	  <label class="control-label col-xs-4">Mobil</label>
-	  <div class="col-xs-6">+49 172 97 36 38 2</div>
-	  <div class="col-xs-2"><a href="tel:+49-172-97-36-38-32"><span class="glyphicon glyphicon-phone pull-right" style="font-size: 2em"></span></a></div>
-        </p>
-      </div>
-      <div class="row">
-        <label class="control-label col-xs-4">E-Mail</label>
-        <p>
-        <div class="col-xs-6">HerzogHermann@aol.com</div>
-        <div class="col-xs-2"><a href="mailto:HerzogHermann@aol.com"><span class="glyphicon glyphicon-envelope pull-right" style="font-size: 2em"></span></a></div>
-        </p>
-      </div>
+    <div class="row" id=phone_row>
+	<label class="control-label col-xs-2" id=phone_label>Telefon</label>
+	<div class="col-xs-8" id=phone_no>+49 9444 97 22 92</div>
+	<div class="col-xs-2" id=phone_ico><a href="tel:+49-9444-97-22-92"><span class="glyphicon glyphicon-earphone pull-right"></span></a></div>
     </div>
+    <div class="row" id=mobil_row>
+	<label class="control-label col-xs-2" id=mobil_label>Mobil</label>
+	<div class="col-xs-8" id=mobil_no>+49 172 97 36 38 2</div>
+	<div class="col-xs-2" id=mobil_ico><a href="tel:+49-172-97-36-38-32"><span class="glyphicon glyphicon-phone pull-right"></span></a></div>
+    </div>
+    <div class="row" id=email_row>
+      <label class="control-label col-xs-2" id=email_label>E&#8209;Mail</label>
+      <div class="col-xs-8" id=email_addr>planungsbuero.herzog@gmail.com</div>
+      <div class="col-xs-2" id=email_ico><a href="mailto:planungsbuero.herzog@gmail.com"><span class="glyphicon glyphicon-envelope pull-right"></span></a></div>
+    </div>
+<div>
